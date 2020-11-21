c538d70a259054c683c3038923ed2c0089279513
Av1352 <66872073+Av1352@users.noreply.github.com>
2020-11-21 13:02:05 +0530

    unmerge

--- patient_form_details/general_details_positive.py
+++ patient_form_details/general_details_positive.py
@@ -22,7 +22,6 @@ class GeneralDetailsPositive:
         self.adhaar = request.form.get('adhaar')
         self.positive_code = positive_code
         self.session_for_general_details_positive(self.positive_code)
-        return session['unique']
 
     def session_for_general_details_positive(self, positive_code):
         session['listOfVariable'] = {}

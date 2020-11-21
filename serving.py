724178d9a3479b372fb32d27076bb750864d787a
Av1352 <66872073+Av1352@users.noreply.github.com>
2020-11-21 11:33:16 +0530

    merge with yaswanthi

--- serving.py
+++ serving.py
@@ -3,19 +3,27 @@ from flask import render_template, request, url_for, redirect, session, make_res
 import sqlite3 as lite
 import os
 
-
+from patient_form_details.general_details_positive import GeneralDetailsPositive
+from patient_form_details.general_details_negative import GeneralDetailsNegative
+from patient_form_details.medical_details import MedicalDetails
+from patient_form_details.code_generation import CodeGeneration
+from patient_form_details.xray_details import XrayDetails
+from patient_form_details.output_generation import OutputGeneration
+# from patient_form_details.session_for_unique_code import SessionForUniqueCode
 app = Flask(__name__)
 app_root = os.path.abspath(os.path.dirname(__file__))
 
-db_path = r"C:\Users\anjuv\Documents\A.T.A.C\database\patient.db"
+db_path = r"C:\Users\yaswanthi\Documents\GitHub\A.T.A.C\database\patient.db"
 
 con = lite.connect(db_path, check_same_thread=False)
 print("db connection successful")
 
-
+app.secret_key = os.urandom(25)
 
 @app.route('/menu_for_forms_positive', methods=['GET', 'POST'])
 def menu_for_forms_positive():
+
+
 	return render_template('menu_for_forms_positive.html')
 
 
@@ -26,40 +34,116 @@ def menu_for_forms_negative():
 
 @app.route('/general_details_positive', methods=['GET', 'POST'])
 def general_details_positive():
-	return render_template('general_details_positive.html')
+	if request.method == "POST":
+		Code_Generation = CodeGeneration()
+		General_Details_Positive = GeneralDetailsPositive(con)
+		session['positive_code'] = Code_Generation.positive_code()
+		General_Details_Positive.general_details_positive_input(session['positive_code'])
 
+		return redirect(url_for('menu_for_forms_positive'))
+
+	if request.method == "GET":
+		return render_template('general_details_positive.html')
 
 @app.route('/general_details_negative', methods=['GET', 'POST'])
 def general_details_negative():
-	return render_template('general_details_negative.html')
+	if request.method == "POST":
+		Code_Generation = CodeGeneration()
+		General_Details_Negative = GeneralDetailsNegative(con)
+		negative_code = Code_Generation.negative_code()
+		General_Details_Negative.general_details_negative_input(negative_code)
+		return redirect(url_for('menu_for_forms_positive'))
+
+	if request.method == "GET":
+		return render_template('general_details_negative.html')
 
 
 @app.route('/output', methods=['GET', 'POST'])
 def output():
-	return render_template('output.html')
+	Calculations_output = OutputGeneration(con)
+	Calculations_output.get_data_from_medical_details()
+
+	# return redirect(url_for('output'))
+	cur = con.cursor()
+	# if request.method == "GET":
+	var = cur.execute("SELECT * FROM general_details WHERE ID = ?", (session['data'],))
+	for row in cur.fetchall():
+		gen_det = row
+	print(gen_det)
+	var = cur.execute("SELECT * FROM medical_details WHERE  unique_code= ?", (session['unique'],))
+	for row in cur.fetchall():
+		med_det = row
+	print(med_det)
+	path_to_photo =session['photo_path']
+	print(path_to_photo)
+
+
+
+
+	return render_template('user_report.html',gen_det=gen_det,med_det=med_det, path_to_photo=path_to_photo)
 
 
 @app.route('/unique_code_positive', methods=['GET', 'POST'])
 def unique_code_positive():
+	cur = con.cursor()
 
-	return render_template('unique_code_positive.html')
+	cursor = cur.execute('SELECT max(id) FROM general_details')
+	max_id = cursor.fetchone()[0]
+	print(max_id)
+	str_id = str(max_id)
+
+	var = cur.execute("SELECT unique_code FROM general_details WHERE ID = ? ", (str_id,))
+	for row in cur.fetchone():
+		session['code_unique'] = row
+	code_unique_new = session['code_unique']
+	print(code_unique_new)
+
+	return render_template('unique_code_positive.html',code_unique_new=code_unique_new)
 
 
 @app.route('/unique_code_negative', methods=['GET', 'POST'])
 def unique_code_negative():
+	cur = con.cursor()
 
-	return render_template('unique_code_negative.html')
+	cursor = cur.execute('SELECT max(id) FROM general_details')
+	max_id = cursor.fetchone()[0]
+	print(max_id)
+	str_id = str(max_id)
+
+	var = cur.execute("SELECT unique_code FROM general_details WHERE ID = ? ", (str_id,))
+	for row in cur.fetchone():
+		session['code_unique'] = row
+	code_unique_new = session['code_unique']
+	print(code_unique_new)
+
+
+	return render_template('unique_code_negative.html', code_unique_new=code_unique_new)
+
+
+
 
 
 @app.route('/medical_details', methods=['GET', 'POST'])
 def medical_details():
-	return render_template('medical_details.html')
+	if request.method == "POST":
+		Medical_Details_Negative = MedicalDetails(con)
+		Medical_Details_Negative.medical_details_input()
+		return redirect(url_for('menu_for_forms_positive'))
+
+	if request.method == "GET":
+		return render_template('medical_details.html')
 
 
 @app.route('/xray_upload', methods=['GET', 'POST'])
 def xray_upload():
+	if request.method == "POST":
+		Xray_Details = XrayDetails(con)
+		Xray_Details.xray_input("unique_code", app_root)
 
-	return render_template('xray_upload.html')
+		return redirect(url_for('menu_for_forms_positive'))
+
+	if request.method == "GET":
+		return render_template('xray_upload.html')
 
 
 @app.route('/')
@@ -112,7 +196,11 @@ def signup():
 @app.route('/thankyou')
 def thankyou():
 
-	return render_template('thankyou.html')
+	fname = request.args.get('fname')
+	lname = request.args.get('lname')
+	return render_template('thankyou.html', fname=fname, lname=lname)
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
